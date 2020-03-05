1c1c902959606db5996c25e68b70d0b920907aa3
unknown <bassam.mmdouh@gmail.com>
2020-03-05 16:20:27 +0200

    Fix access deny issue and Auth lambda

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'lqwwddgi18'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-7abydt13.auth0.com',
+  clientId: '2DzBCmCEHXIWiH9bzDAwlTYRwjVz9aDh',
+  callbackUrl: 'http://localhost:3000/callback'
+}
